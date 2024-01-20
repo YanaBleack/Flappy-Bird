01c7c308a619f84732d9a6634f3e178f85355ebb
Yana <119619850+YanaBleack@users.noreply.github.com>
2024-01-20 19:02:07 +0300

    Update BirdCollision.cs

--- Scripts/Player/BirdCollision.cs
+++ Scripts/Player/BirdCollision.cs
@@ -14,13 +14,9 @@ public class BirdCollision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.TryGetComponent(out ScoreZone scoreZone))
-        {
-            _bird.IncreaseScore();
-        }
-        else
-        {
-            _bird.Die();
-        }
+        if(collision.TryGetComponent(out ScoreZone scoreZone))        
+            _bird.IncreaseScore();        
+        else        
+            _bird.Die();        
     }
 }
