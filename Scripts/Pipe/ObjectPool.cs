e6351548ab15976e242d1fb768fae2c16fb75322
Yana <119619850+YanaBleack@users.noreply.github.com>
2024-01-21 00:42:12 +0300

    Update ObjectPool.cs

--- Scripts/Pipe/ObjectPool.cs
+++ Scripts/Pipe/ObjectPool.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class ObjectPool<T> where T : MonoBehaviour
 {
-    protected List<T> _îbjects = new List<T>();
+    protected List<T> _Ã®bjects = new List<T>();
 
     public ObjectPool(List<T> objects)
     {
-        _îbjects = objects;
+        _Ã®bjects = objects;
     }
 
     public bool TrySpawn(Vector3 position, out T spawned)
-    {
-       
-        foreach (var obj in _îbjects)
+    {       
+        foreach (var obj in _Ã®bjects)
         {
             if (obj.gameObject. activeSelf == false)
             {
@@ -30,11 +29,11 @@ public class ObjectPool<T> where T : MonoBehaviour
         return false;
     }
 
-    public List<T> GetObjects() => new List<T>(_îbjects);
+    public List<T> GetObjects() => new List<T>(_Ã®bjects);
 
     public void Reset()
     {
-        foreach (var obj in _îbjects)
+        foreach (var obj in _Ã®bjects)
         {
             obj.gameObject.SetActive(false);
         }
